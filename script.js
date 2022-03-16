8cfaa8d631dde63626fd7bb95c2525d541361004
dev-fominov <dev-fominov@yandex.by>
2022-03-16 18:48:49 +0300

    Pillars

--- script.js
+++ script.js
@@ -31,3 +31,11 @@ function powersOfTwo(n){
 function usdcny(usd) {
   return (usd * 6.75).toFixed(2) + ' Chinese Yuan';
 }
+
+function pillars(num_pill, dist, width) {
+  if(num_pill > 1){
+    return num_pill*width - 2*width + (num_pill-1)*dist/0.01;
+  } else {
+    return 0;
+  }
+}
