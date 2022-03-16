049a14ca5d66939add92fc741d55da6043a975b2
dev-fominov <dev-fominov@yandex.by>
2022-03-16 18:01:02 +0300

    Powers of two

--- script.js
+++ script.js
@@ -19,3 +19,11 @@ function lovefunc(flower1, flower2){
     return true;
   }
 }
+
+function powersOfTwo(n){
+  let arr = [];
+  for(let i = 0; i <= n; i++ ) {
+    arr.push(Math.pow(2, i));
+  }
+  return arr;
+}
